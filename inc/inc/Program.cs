d318fd79a8edcba4fda16dbf1039534fb3e21881
chaitanchun <chaitanchun@gmail.com>
2014-03-22 20:36:13 +0800

    change code

--- inc/inc/Program.cs
+++ inc/inc/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.TeamFoundation.VersionControl;
 using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using System.IO;
 
 namespace inc
 {
@@ -16,12 +17,68 @@ namespace inc
         String idfile = "";
         String verfile = "";
         String maxKeyword = "";
-
+        String uri = "";
         String minKeywork = "";
+        VersionControlServer vcServer = null;
+        Workspace ws = null;
+        String maxNum;
+        String minNum;
 
-        public bool ChangeBuildID(String file, String max, String min)
+        public Program(String max, String min)
         {
+            maxNum = max;
+            minNum = min;
+        }
+
+        public bool ConnectTFS()
+        {
+            TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(new Uri(uri));
+            vcServer = tpc.GetService<VersionControlServer>();
+            if (vcServer == null)
+                return false;
+            WorkspaceInfo wsInfo = Workstation.Current.GetLocalWorkspaceInfo(vcServer, @"workspacename", @"UserName");
+            if(wsInfo == null)
+                return false;
+            ws = vcServer.GetWorkspace(wsInfo);
             return true;
+        }
+
+        public bool ChangeBuildID(String file)
+        {
+            //checkout fils
+            try
+            {
+                if (ws.PendEdit(file) != 1)
+                    return false;
+
+                //Edit file based on current id
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error to edit file {1} :{0}", e.Message, file);
+                return false;
+            }
+            return true;
+        }
+
+        public void CheckIn()
+        {
+            String id = "M0";
+            if (maxNum.Length == 1)
+                id += "0";
+            id += maxNum + "U0";
+            if (minNum.Length == 1)
+                id += "0";
+            id += minNum;
+            
+            // Get the pending change, and check in the new revision.
+            var pendingChanges = ws.GetPendingChanges();
+            int changesetForChange = ws.CheckIn(pendingChanges, "Change Build ID to " + id);
+            if (changesetForChange > 0)
+                Console.WriteLine("Checked in changeset {0}", changesetForChange);
+            else
+                Console.WriteLine("Submit change is fail!");
         }
 
         static void Main(string[] args)
@@ -54,8 +111,15 @@ namespace inc
                 return;
             }
 
-            Program program = new Program();
+            Program program = new Program(args[0], args[1]);
+            if( !program.ConnectTFS() )
+            {
+                Console.WriteLine("Connect TFS is fail!");
+                return;
+            }
 
+
+            return;
         }
     }
 }
